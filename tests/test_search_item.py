9a9a745b01bf57eb85ab1cc03da7b23f639a8515
arkadijsyckov <Z4mmer@gmail.com>
2025-06-03 03:10:08 +0500

    17

--- tests/test_search_item.py
+++ tests/test_search_item.py
@@ -2,7 +2,7 @@ import allure
 from pageobject.search_item import search
 
 @allure.story('Выбор магазина для самовывоза')
-def test_successful_choose_address():
+def test_successful_search_item():
     search.open_store().close_popap().search_tap().search_item().verify_search_results()
 
 
